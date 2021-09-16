e660e32ffb1ef03dd300d7b04eae47f7e345f3bb
Jake Bathman <jake.bathman@gmail.com>
2021-09-16 14:28:36 -0500

    Height fix #3

--- index.js
+++ index.js
@@ -207,12 +207,14 @@ class Name {
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.getElementById('height').innerHTML = window.innerHeight;
 
 // We listen to the resize event
 window.addEventListener('resize', () => {
     // We execute the same script as before
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+    document.getElementById('height').innerHTML = window.innerHeight;
 });
 
 // Key listener for generating a new name
